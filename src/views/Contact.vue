0430641438a15d7b3ce28ef2d91a67d0fc438f90
korkot21032548-ux <korkot21032548@gmail.com>
2026-01-11 16:48:06 +0700

    update v1

---
+++ src/views/Contact.vue
@@ -0,0 +1,8 @@
+<template >
+    <div>
+        <h1>Contact to korakot khumsup</h1>
+        <h3>tel 086-1581266</h3>
+        <img alt="Vue logo" src="../assets/s1.webp">
+    </div>
+</template>
+
