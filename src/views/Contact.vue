f0f385b9ec2acd0151362b5ffbc12dc14a483ff9
korkot21032548-ux <korkot21032548@gmail.com>
2026-01-11 18:57:29 +0700

    update v1

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,29 +1,62 @@
 <template >
-    <div>
-        <h1>Contact to korakot khumsup</h1>
-        <h3>tel 086-1581266</h3>
-        <img alt="Vue logo" src="../assets/s1.webp">
+    <div class="container">
+         <h1>Contact to korkot khumsup</h1>
+         <h3>Tel.086-1581266</h3>
+         <img alt="Vue logo"  height="50%" src="../assets/s1.webp">
+<br><br><br>
 
-        <div class="mb-3">
+
+<div class="container">
+    <div class="row">
+    <div class="col-sm-6">
+         <img alt="Vue logo" width="100%" src="../assets/s1.webp">
+      </div>
+
+    <div class="col-sm-6 bg-body-tertiary">
+    <div class="mb-3 mt-4">
   <label for="exampleFormControlInput1" class="form-label">Email address</label>
   <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
 </div>
 <div class="mb-3">
   <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
   <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-
+</div>
+ <div class="mb-3 text-center">
+  <button type="button" class="btn btn-primary me-2">Submit</button>
+  <button type="button" class="btn btn-danger">Cancle</button>
 </div>
 
-<button type="button" class="btn btn-primary">submit</button>
-<button type="button" class="btn btn-danger">cencel</button>
 
-    </div>
+</div>
+  </div>
+</div>
+</div>
 </template>
 
-<style scope>
-h1{
-    color: #24a55e;
-}
-</style>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
